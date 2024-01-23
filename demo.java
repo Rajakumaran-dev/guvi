408345f439c5d5652e286fe5d7315b586cd61304
Rajakumaran-dev <152769459+Rajakumaran-dev@users.noreply.github.com>
2024-01-23 12:59:12 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,6 @@
+public class Jenkins {
+    public static void main(String[] args) {
+        // Using System.out.println to print a message
+        System.out.println("Hello, Jenkins!");
+    }
+}
